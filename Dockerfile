965029bb16a7d2f4deb8d4f3a1dff5e628cab941
Ben Elliot <benjiboi214@gmail.com>
2019-11-10 21:53:00 +1100

    Personal build of jenkins image that lets us do some config file magic before booting the container.

---
+++ Dockerfile
@@ -0,0 +1,43 @@
+FROM jenkins/jenkins
+
+## Setup AWS S3 Access credentials for config pull
+ARG AWS_ACCESS_KEY_ID
+ARG AWS_SECRET_ACCESS_KEY
+
+ENV AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
+ENV AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
+
+ARG uid=1000
+ARG gid=1000
+ARG user=jenkins
+
+## Switch to root user to install awscli -- https://github.com/WASdev/ci.docker/issues/194#issuecomment-433519379
+USER root
+
+# Install JQ
+RUN apt-get update && \
+    apt-get install -y jq
+
+## Install AWS CLI cleanly -- https://stackoverflow.com/a/46049066
+RUN apt-get update && \
+    apt-get install -y \
+        python3 \
+        python3-pip \
+        python3-setuptools \
+        groff \
+        less \
+    && pip3 install --upgrade pip \
+    && apt-get clean
+
+RUN pip3 --no-cache-dir install --upgrade awscli
+
+## Copy across entry point script
+COPY ./entrypoint.sh /entrypoint
+RUN sed -i 's/\r$//g' /entrypoint
+RUN chmod +x /entrypoint
+RUN chown ${user} /entrypoint
+
+## Switch back to jenkins user
+USER ${user}
+
+ENTRYPOINT ["/entrypoint"]
---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+export JENKINS_HOME="/var/jenkins_home"
+export AWS_BUCKET_NAME="${AWS_BUCKET_NAME:-"systemiphus-jenkins-config-backups"}"
+export AWS_BUCKET_KEY=`aws s3api list-objects --bucket "systemiphus-jenkins-config-backups" |jq  -c ".[] | max_by(.LastModified)|.Key"`
+export AWS_BUCKET_KEY="${AWS_BUCKET_KEY%\"}"
+export AWS_BUCKET_KEY="${AWS_BUCKET_KEY#\"}"
+export CONFIG_TAR_PATH="$JENKINS_HOME/$AWS_BUCKET_KEY"
+# export CONFIG_FOLDER_PATH="$JENKINS_HOME/$AWS_BUCKET_KEY" ## For when the key is the same as the folder that it was created in
+export CONFIG_FOLDER_PATH="$JENKINS_HOME/config.bak"
+
+## Get tarball from S3 ##
+echo "INFO: Getting Config Tarball"
+if aws s3api get-object --bucket="$AWS_BUCKET_NAME" --key $AWS_BUCKET_KEY $CONFIG_TAR_PATH; then
+    
+    ## Unpack tarball ##
+    echo "INFO: Got Tarball, Unpacking"
+    tar -xvzf $CONFIG_TAR_PATH -C $JENKINS_HOME/
+    ls -la $CONFIG_FOLDER_PATH
+
+    ## Put it in the jenkins home dir ##
+    echo "INFO: Move config to jenkins dir"
+    # Copy global configuration files into the workspace
+    cp $CONFIG_FOLDER_PATH/*.xml $JENKINS_HOME/
+    # Copy keys and secrets into the workspace
+    cp $CONFIG_FOLDER_PATH/identity.key.enc $JENKINS_HOME/
+    cp $CONFIG_FOLDER_PATH/secret.key $JENKINS_HOME/
+    cp $CONFIG_FOLDER_PATH/secret.key.not-so-secret $JENKINS_HOME/
+    cp -r $CONFIG_FOLDER_PATH/secrets $JENKINS_HOME/
+
+    # Copy user configuration files into the workspace
+    cp -r $CONFIG_FOLDER_PATH/users $JENKINS_HOME/
+
+    # Copy job definitions into the workspace
+    cp -R $CONFIG_FOLDER_PATH/jobs/ $JENKINS_HOME/
+
+    # Copy plugins (I added S3)
+    cp -R $CONFIG_FOLDER_PATH/plugins $JENKINS_HOME
+
+    ## Remove tarball and tmp resources ##
+    echo "INFO: Remove Tarball and TMP"
+    rm -rf $CONFIG_TAR_PATH
+    rm -rf $CONFIG_FOLDER_PATH
+
+else
+
+    echo "ERROR: Could not get Tarball"
+
+fi
+
+## Start Jenkins ##
+echo "INFO: Start Jenkins!"
+exec /sbin/tini -- /usr/local/bin/jenkins.sh "$@"
